13cd7161cc3d5b7e07b1fd99fb86ea195f6f1548
dhanamamidipaka585 <40660467+dhanamamidipaka585@users.noreply.github.com>
2026-02-16 12:01:04 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,7 @@ try:
     
 except Exception as ex:
     st.error(f"An error occurred: {str(ex)}")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
